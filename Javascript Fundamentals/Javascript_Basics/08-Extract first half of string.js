3183b51a4f2d7f56e5f1b1798544fa1f04aca7a5
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-29 22:02:21 +0530

    JS Challenger

---
+++ Javascript Fundamentals/Javascript_Basics/08-Extract first half of string.js
@@ -0,0 +1,7 @@
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+function myFunction(a) {
+  let half = a.length / 2;
+  return a.slice(0, half);
+}
