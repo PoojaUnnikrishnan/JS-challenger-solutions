27a3591921fda879a9aa816429abb94599e684b5
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-31 15:44:05 +0530

    JS Challenger

---
+++ Javascript Fundamentals/Javascript_dates/02-Return the number of days between two dates.js
@@ -0,0 +1,5 @@
+// Write a function that takes two date instances as argument
+// It should return the number of days that lies between those dates
+function myFunction(a, b) {
+  return Math.abs(a - b) / (24 * 60 * 60 * 1000);
+}
