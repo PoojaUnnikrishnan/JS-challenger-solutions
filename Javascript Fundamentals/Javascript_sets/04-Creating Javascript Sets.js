b4605f425668ce0213ab3996e709bdebadb91447
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-31 15:46:56 +0530

    JS Challenger

---
+++ Javascript Fundamentals/Javascript_sets/04-Creating Javascript Sets.js
@@ -0,0 +1,7 @@
+// Write a function that takes three elements of any type as arguments
+// Create a Set from those elements
+// Return the result
+function myFunction(a, b, c) {
+  let sets = new Set([a, b, c]);
+  return sets;
+}
